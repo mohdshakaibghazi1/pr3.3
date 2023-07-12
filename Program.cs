153a09d6a2887681017ca207e5ee0e335d4cbc63
Mohd Shakaib Ghazi <mohd.shakaibghazi@mphasis.com>
2023-07-12 15:36:18 +0530

    commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string decimalStr = "1.34";
+            try
+            {
+                decimal convertedDecimal = decimal.Parse(decimalStr);
+                Console.WriteLine("Converted decimal: " + convertedDecimal);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error!!!-----Pls give correct one.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Error: Decimal value is too large or too small");
+
+            }
+        }
+    }
+}
